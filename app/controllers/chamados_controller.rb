1dc4a66ce6e7a4335e1668f78eff70151454ad88
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2012-07-21 10:16:14 -0300

    Removendo codigo que nao sera utilizado nesse momento

--- app/controllers/chamados_controller.rb
+++ app/controllers/chamados_controller.rb
@@ -12,12 +12,6 @@ class ChamadosController < ApplicationController
     @chamado_json = @chamado.to_json(:only => [:sistema_id, :contato_id, :cliente_id, :titulo, :descricao,:responsavel_id, :status_id, :natureza_id,])
   end
   
-  def adicionar_tramite
-    @chamado = Chamado.find params[:id]
-    @chamado.tramite = Tramite.new if @chamado.tramite == nil
-    @chamado_json = @chamado.to_json(:only => [:sistema_id, :contato_id, :cliente_id, :titulo, :descricao,:responsavel_id, :status_id, :natureza_id,])
-  end
-  
   def create
     @chamado = Chamado.new
     @chamado.save
